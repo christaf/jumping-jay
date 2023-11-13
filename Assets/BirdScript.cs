d81d7e08b346f63caf0593ee1ff3fe756aa3b7fe
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-11-13 19:22:01 +0100

    Added few new buttons to control movement, added OnCollide event handler, added isAlive property, added GameOverScreen and setting it at the proper time and place

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -3,18 +3,62 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
+    public LogicScript logicScript;
+
+    public bool isAlive;
+
+    private void Start()
+    {
+        isAlive = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isAlive)
         {
-            myRigidBody.velocity = Vector2.up * 10;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                myRigidBody.velocity = Vector2.up * 10;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                myRigidBody.velocity = Vector2.right * 2;
+            }
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                myRigidBody.velocity = Vector2.left * 2;
+            }
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                myRigidBody.velocity = Vector2.up * 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                myRigidBody.velocity = Vector2.down * 2;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            logicScript.GameOver();
+            isAlive = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            myRigidBody.velocity = Vector2.right * 10;
+            logicScript.RestartGame();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "BottomPipe" || col.gameObject.name == "TopPipe")
+        {
+            logicScript.GameOver();
+            isAlive = false;
         }
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
-
+    public GameObject gameOverScreen;
     private void Start()
     {
-        playerScore = 0;
+        ResetScore();
+        gameOverScreen.SetActive(false);
     }
-
+    
     [ContextMenu("Add Score")]
     public void AddScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
-        scoreText.text = "score: " + playerScore.ToString();
+        scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    [ContextMenu("Reset Score")]
+    public void ResetScore()
+    {
+        playerScore = 0;
+        scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    [ContextMenu("Restart Game")]
+    public void RestartGame()
+    {
+        ResetScore();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Restart Game");
+        // Application.LoadLevel(Application.loadedLevel);
+    }
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        Debug.Log("Game Over");
     }
 
 }
