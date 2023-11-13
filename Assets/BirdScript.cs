fdd4633873fb92ea2ab70861865e7bb186eb4953
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-11-13 19:56:18 +0100

    Added static borders of the gameboard

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody;
+    public Rigidbody2D birdRigidBody;
     public LogicScript logicScript;
 
     public bool isAlive;
@@ -15,43 +15,57 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isAlive)
+        if (!isAlive)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                myRigidBody.velocity = Vector2.up * 10;
+                logicScript.GameOver();
+                isAlive = false;
             }
 
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetKeyDown(KeyCode.R))
             {
-                myRigidBody.velocity = Vector2.right * 2;
-            }
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                myRigidBody.velocity = Vector2.left * 2;
-            }
-            if(Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                myRigidBody.velocity = Vector2.up * 2;
+                logicScript.RestartGame();
             }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                myRigidBody.velocity = Vector2.down * 2;
-            }
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            birdRigidBody.velocity = Vector2.up * 7;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            birdRigidBody.velocity = Vector2.right * 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            birdRigidBody.velocity = Vector2.left * 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            birdRigidBody.velocity = Vector2.up * 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            birdRigidBody.velocity = Vector2.down * 2;
         }
         
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (birdRigidBody.transform.position.y > 10 || birdRigidBody.transform.position.y < -10)
         {
             logicScript.GameOver();
             isAlive = false;
         }
-
-        if (Input.GetKeyDown(KeyCode.R))
+        
+        if(birdRigidBody.transform.position.x > 17 || birdRigidBody.transform.position.x < -17)
         {
-            logicScript.RestartGame();
+            logicScript.GameOver();
+            isAlive = false;
         }
+        Debug.Log("Bird Coordinates: " + birdRigidBody.transform.position);
     }
+
 
     private void OnCollisionEnter2D(Collision2D col)
     {
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -4,11 +4,41 @@ using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
 {
+    private Camera _mainCamera;
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+
+    private void GetCameraEdgesCoordinates()
+    {
+        if (_mainCamera == null)
+        {
+            Debug.LogError("Main camera not found!");
+            return;
+        }
+
+        float cameraHeight = 2f * _mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * _mainCamera.aspect;
+
+        float cameraX = _mainCamera.transform.position.x;
+        float cameraY = _mainCamera.transform.position.y;
+
+        float cameraLeftEdge = cameraX - cameraWidth / 2;
+        float cameraRightEdge = cameraX + cameraWidth / 2;
+        float cameraTopEdge = cameraY + cameraHeight / 2;
+        float cameraBottomEdge = cameraY - cameraHeight / 2;
+
+        Debug.Log("Left Edge: " + cameraLeftEdge);
+        Debug.Log("Right Edge: " + cameraRightEdge);
+        Debug.Log("Top Edge: " + cameraTopEdge);
+        Debug.Log("Bottom Edge: " + cameraBottomEdge);
+    }
     private void Start()
     {
+        _mainCamera = Camera.main;
+
+        // Call this method to get and print the coordinates of camera edges
+        GetCameraEdgesCoordinates();
         ResetScore();
         gameOverScreen.SetActive(false);
     }
